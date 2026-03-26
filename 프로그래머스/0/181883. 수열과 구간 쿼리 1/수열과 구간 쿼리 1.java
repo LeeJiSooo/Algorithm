afc24e2ca26d6c4e64015124dc48fa59c812a62e
이지수 <147130052+LeeJiSooo@users.noreply.github.com>
2026-03-26 20:20:36 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.95 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        for(int i = 0; i < queries.length; i++){
+            int s = queries[i][0];
+            int e = queries[i][1];
+            
+            for(int j = s; j <= e; j++){
+                arr[j] = arr[j] + 1;
+            }
+        }
+        
+        int[] answer = arr.clone();
+        
+        return answer;
+        
+    }
+}
