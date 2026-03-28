0b1bb2a4c0ac9074fc402a528c3d1c6e357979f3
이지수 <147130052+LeeJiSooo@users.noreply.github.com>
2026-03-28 22:12:36 +0900

    [level 0] Title: 배열 만들기 5, Time: 5.61 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < intStrs.length; i++){
+            String temp = intStrs[i];
+            String point = temp.substring(s, s + l);
+            int num = Integer.parseInt(point);
+            
+            if(num > k){
+                list.add(num);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
