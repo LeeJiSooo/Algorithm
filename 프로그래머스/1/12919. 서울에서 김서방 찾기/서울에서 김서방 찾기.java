09be3199b3caff4e80e5a1dbf6faaf52652c05c7
이지수 <147130052+LeeJiSooo@users.noreply.github.com>
2026-03-30 16:21:08 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 10.14 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] seoul) {        
+        int num = 0;
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                num = i;
+                break;
+            }
+        }
+        String answer = "김서방은 " + num + "에 있다";
+        return answer;
+    }
+}
