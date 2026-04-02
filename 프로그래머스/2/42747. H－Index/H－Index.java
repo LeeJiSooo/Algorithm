eb0435309f2275cb12cd7f23e53cbde015429604
이지수 <147130052+LeeJiSooo@users.noreply.github.com>
2026-04-02 20:22:25 +0900

    [level 2] Title: H-Index, Time: 1.03 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        
+        for(int i = 0; i < citations.length / 2; i++){
+            int temp = citations[i];
+            citations[i] = citations[citations.length - 1 - i];
+            citations[citations.length - 1 - i] = temp;
+        }
+        
+        int hIndex = 0;
+        for(int i = 0; i < citations.length; i++){
+            if(citations[i] >= i + 1){
+                hIndex = i + 1;
+            }else{
+                break;
+            }
+        }
+        
+        return hIndex;
+    }
+}
