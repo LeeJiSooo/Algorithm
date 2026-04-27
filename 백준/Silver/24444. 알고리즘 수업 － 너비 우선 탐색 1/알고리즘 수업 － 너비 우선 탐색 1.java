b31c40e73085e04148fe82978de693e8af5715e2
이지수 <147130052+LeeJiSooo@users.noreply.github.com>
2026-04-27 16:22:14 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1608 ms, Memory: 327356 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static int[] check;
+    static int count = 1;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int r = sc.nextInt();
+
+        visited = new boolean[n + 1];
+        check = new int[n + 1];
+
+        for(int i = 0; i <= n; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+
+        for(int i = 0; i < m; i++){
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+
+            graph.get(s).add(e);
+            graph.get(e).add(s);
+        }
+
+        for(int i = 1; i <= n; i++){
+            Collections.sort(graph.get(i));
+        }
+
+        bfs(r);
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i <= n; i++){
+            sb.append(check[i]).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+    public static void bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+
+        visited[start] = true;
+        check[start] = count++;
+        q.offer(start);
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for(int next : graph.get(node)){
+                if(!visited[next]){
+                    visited[next] = true;
+                    check[next] = count++;
+                    q.offer(next);
+                }
+            }
+        }
+    }
+}
