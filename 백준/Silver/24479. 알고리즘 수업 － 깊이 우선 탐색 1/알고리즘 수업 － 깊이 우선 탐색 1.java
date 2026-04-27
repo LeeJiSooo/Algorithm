39000af381f55498fcd3a4b3a754eda239b2fabd
이지수 <147130052+LeeJiSooo@users.noreply.github.com>
2026-04-27 14:33:03 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1584 ms, Memory: 330356 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,57 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static int[] check;
+    static int count = 1;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int R = sc.nextInt();
+
+        visited = new boolean[N + 1];
+        check = new int[N + 1];
+
+        for(int i = 0; i <= N; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+
+        for(int i = 0; i < M; i++){
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+            graph.get(s).add(e);
+            graph.get(e).add(s);
+        }
+
+        for(int i = 1; i <= N; i++){
+            Collections.sort(graph.get(i));
+        }
+
+        dfs(R);
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i <= N; i++){
+            sb.append(check[i]).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static void dfs(int node){
+        visited[node] = true;
+        check[node] = count++;
+
+        for(int next : graph.get(node)){
+            if(!visited[next]){
+                dfs(next);
+            }
+        }
+    }
+}
+
